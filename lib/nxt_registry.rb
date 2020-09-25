a8dd0706e2162a5ef985ccfed2acd2758c686541
Andreas Robecke <a.robecke@getsafe.de>
2020-09-25 23:23:48 +0200

    Extract method

--- lib/nxt_registry.rb
+++ lib/nxt_registry.rb
@@ -1,30 +1,30 @@
 require 'active_support/core_ext'
-require "nxt_registry/version"
-require "nxt_registry/blank"
-require "nxt_registry/attribute"
-require "nxt_registry/errors"
-require "nxt_registry/registry_builder"
-require "nxt_registry/registry"
-require "nxt_registry/recursive_registry"
+require 'nxt_registry/version'
+require 'nxt_registry/blank'
+require 'nxt_registry/attribute'
+require 'nxt_registry/errors'
+require 'nxt_registry/registry_builder'
+require 'nxt_registry/registry'
+require 'nxt_registry/recursive_registry'
 
 module NxtRegistry
   def registry(name, **options, &config)
-    return registries.fetch(name) if registries.key?(name)
-
-    registry = Registry.new(name, **options, &config)
-    registries[name] ||= registry
-    registry
+    build_registry(Registry, name, **options, &config)
   end
 
   def recursive_registry(name, **options, &config)
-    return registries.fetch(name) if registries.key?(name)
-
-    registry = RecursiveRegistry.new(name, **options, &config)
-    registries[name] ||= registry
-    registry
+    build_registry(RecursiveRegistry, name, **options, &config)
   end
 
   private
+
+  def build_registry(registry_class, name, **options, &config)
+    return registries.fetch(name) if registries.key?(name)
+
+    registry = registry_class.new(name, **options, &config)
+    registries[name] ||= registry
+    registry
+  end
 
   def registries
     @registries ||= {}
